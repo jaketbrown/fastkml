ea8943dae8c9ce5bb10afbc431d1392f92ce7182
jake <jake@ubuntudev.myguest.virtualbox.org>
2023-06-03 18:05:05 -0400

    Add more coverage

--- mayhem/fastkml_fuzz.py
+++ mayhem/fastkml_fuzz.py
@@ -3,10 +3,7 @@ import atheris
 import sys
 import datetime
 
-from dateutil.tz import tzoffset
-from dateutil.tz import tzutc
-
-with atheris.instrument_imports(include=['aicsimageio']):
+with atheris.instrument_imports(include=['fastkml']):
     import fastkml as kml
     from fastkml.enums import DateTimeResolution
     from fastkml.times import KmlDateTime
@@ -18,11 +15,32 @@ def TestOneInput(data):
     year = fdp.ConsumeIntInRange(1900, 2023)
     day = fdp.ConsumeIntInRange(1, 28)
     month = fdp.ConsumeIntInRange(1, 12)
+    choice = fdp.ConsumeIntInRange(0, 3)
+    consumed_bytes = fdp.ConsumeBytes(fdp.remaining_bytes())
     try:
-        dt = datetime.datetime(year=year, day=day, month=month)
-        kdt = KmlDateTime(dt, DateTimeResolution.year)
-    except Exception:
+        if choice == 1:
+            dt = datetime.datetime(year=year, day=day, month=month)
+            kdt = KmlDateTime(dt, DateTimeResolution.year)
+        else:
+            # Create the root KML object
+            k = kml.KML()
+            ns = "{http://www.opengis.net/kml/2.2}"  # noqa: FS003
+
+            # Create a KML Document and add it to the KML root object
+            d = kml.Document(ns, "docid", "doc name", consumed_bytes.decode('utf-8'))
+            k.append(d)
+
+            # Create a KML Folder and add it to the Document
+            f = kml.Folder(ns, "fid", "f name", consumed_bytes.decode('utf-8'))
+            d.append(f)
+
+            # Create a KML Folder and nest it in the first Folder
+            nf = kml.Folder(ns, "nested-fid", "nested f name", consumed_bytes.decode('utf-8'))
+            f.append(nf)
+    except UnicodeDecodeError:
         return
+    except Exception:
+        raise
 
 def main():
     atheris.Setup(sys.argv, TestOneInput)
